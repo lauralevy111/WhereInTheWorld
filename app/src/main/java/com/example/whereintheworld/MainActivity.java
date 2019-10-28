94d35bcb2ba4a88e59198d79f4ba9339ff09ef4b
Laura Levy <llevy510@gmail.com>
2019-10-28 14:18:53 -0400

    * created SettingsActivity.java * created aciivity_settings.xml * created to_settings_button button in activity_main.xml * created listener to send to SettingsActivity in MainActivity

--- app/src/main/java/com/example/whereintheworld/MainActivity.java
+++ app/src/main/java/com/example/whereintheworld/MainActivity.java
@@ -1,4 +1,29 @@
 package com.example.whereintheworld;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button toMapButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acticity_main);
+
+        toMapButton = findViewById(R.id.toMapActivityButton);
+        toMapButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent (getApplicationContext(),MapsActivity.class);
+                //
+                startActivity(intent);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/whereintheworld/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.whereintheworld;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public Button toMapButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
