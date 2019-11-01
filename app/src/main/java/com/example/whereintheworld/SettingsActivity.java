8872c3af9054c2349708b82212261a68673b7c24
Laura Levy <llevy510@gmail.com>
2019-11-01 08:52:41 -0400

    * added switches in SettingsActivity.java

--- app/src/main/java/com/example/whereintheworld/SettingsActivity.java
+++ app/src/main/java/com/example/whereintheworld/SettingsActivity.java
@@ -1,17 +1,27 @@
 package com.example.whereintheworld;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    //ublic Button toMapButton;
+    public Switch bathroomSwitch;///bathroom_switch
+    public Switch foodKitchenSwitch;///bathroom_switch
+    public Switch shelterSwitch;///bathroom_switch
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_settings);
+
+        bathroomSwitch = findViewById(R.id.bathroom_switch);
+        foodKitchenSwitch = findViewById(R.id.foodKitchen_switch);
+        shelterSwitch = findViewById(R.id.shelter_switch);
+
+
 
     }
 
