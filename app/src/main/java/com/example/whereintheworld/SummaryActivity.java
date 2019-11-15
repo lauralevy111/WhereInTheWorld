ecd085a7416503a373ce6d3cdcdcedd4d6f2c122
Laura Levy <llevy510@gmail.com>
2019-11-15 07:47:39 -0500

    * working on the ui for activity_summary.xml  * added summaryActivity to AndroidManifest

--- app/src/main/java/com/example/whereintheworld/SummaryActivity.java
+++ app/src/main/java/com/example/whereintheworld/SummaryActivity.java
@@ -2,6 +2,7 @@ package com.example.whereintheworld;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +10,19 @@ public class SummaryActivity extends AppCompatActivity {
 
     public Intent intent;
 
+    public TextView bigTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.intent =getIntent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        bigTextView= findViewById(R.id.bigTextView);
+
+        String bigTextFill = "XXXXXXX\n XXXXXXXX\n XXXXX\nXXXXX\nXXXXX\nSSSXXX\nXXXXXX";
+
+        bigTextView.setText(bigTextFill);
 
     }
 }
