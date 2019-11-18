31a7a1e8bc04c9d55213f2407ed2883fce817cb3
Laura Levy <llevy510@gmail.com>
2019-11-18 08:39:32 -0500

    * created user and hub objects

---
+++ app/src/main/java/com/example/whereintheworld/Hub.java
@@ -0,0 +1,14 @@
+package com.example.whereintheworld;
+
+public class Hub {
+
+    private String name;
+    private int longitude;
+    private int latitude;
+
+    public Hub(String name, int longitude, int latitude){
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude=latitude;
+    }
+}
---
+++ app/src/main/java/com/example/whereintheworld/User.java
@@ -0,0 +1,10 @@
+package com.example.whereintheworld;
+
+public class User {
+
+    private String name;
+    private int id;
+    private String email;
+
+    public User(String name, String email){this.name = name; this.email = email;}
+}
