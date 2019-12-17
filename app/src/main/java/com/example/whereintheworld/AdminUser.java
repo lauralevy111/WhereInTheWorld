9a0b80ea0a57778d4a4b8f7fc73a53a02ae8cfbc
Laura Levy <llevy510@gmail.com>
2019-12-17 08:25:42 -0500

    * AdminUser.java : added void method createHub()

--- app/src/main/java/com/example/whereintheworld/AdminUser.java
+++ app/src/main/java/com/example/whereintheworld/AdminUser.java
@@ -3,5 +3,12 @@ package com.example.whereintheworld;
 public class AdminUser extends User {
     public AdminUser(String name, String email, long adminID){
         super(name,email);
+
+
+    }
+
+    public void createHub(String name, int longitude, int lattitude){
+        Hub newHub = new Hub (name,longitude,lattitude);
+
     }
 }
