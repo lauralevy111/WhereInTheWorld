2cb7af01e5b398cdfb1a9468e83abad6f6d8aa06
Laura Levy <llevy510@gmail.com>
2019-12-20 08:32:18 -0500

    * BusinessUser.java :  ** created method bill

--- app/src/main/java/com/example/whereintheworld/BusinessUser.java
+++ app/src/main/java/com/example/whereintheworld/BusinessUser.java
@@ -4,8 +4,16 @@ public class BusinessUser extends User{
 
     public int businessID;
 
+    public double invoice;
+
     public BusinessUser(String name, String email, int businessID){
         super(name, email);
+    }
+
+    public void  bill(double amount) {
+        invoice += amount;
+        System.out.println("pls pay us ? this amount : "+invoice+amount);
+
     }
 
 
